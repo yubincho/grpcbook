896a7f3d22828aebc97122019353dade6cd6c03e
yubincho <yubincho9@gmail.com>
2025-03-02 14:03:10 +0900

    first commit

---
+++ src/main/java/com/example/grpcbook/GrpcbookApplication.java
@@ -0,0 +1,14 @@
+package com.example.grpcbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcbookApplication.class, args);
+	}
+
+}
+
